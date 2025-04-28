5a23331056601d1ef5cdbf975174daad639114fc
zhanghaoyu01 <zhanghaoyu01@zhuanzhuan.com>
2025-04-28 18:56:20 +0800

    code

--- src/main/java/org/example/springmcpserver/config/McpConfig.java
+++ src/main/java/org/example/springmcpserver/config/McpConfig.java
@@ -1,7 +1,9 @@
 package org.example.springmcpserver.config;
 
 import org.example.springmcpserver.service.WeatherService;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.function.FunctionToolCallback;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,4 +21,20 @@ public class McpConfig {
     public ToolCallbackProvider getToolCallbackProvider(WeatherService weatherService) {
         return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
     }
+
+    @Bean
+    public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+        return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+    }
+
+//    public record TextInput(String input) {
+//    }
+//
+//    @Bean
+//    public ToolCallback toUpperCase() {
+//        return FunctionToolCallback.builder("toUpperCase", (TextInput input) -> input.input().toUpperCase())
+//                .inputType(TextInput.class)
+//                .description("Put the text to upper case")
+//                .build();
+//    }
 }
