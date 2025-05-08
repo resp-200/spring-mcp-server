3de32eee37eeec8969a3963578455518fbf0254a
zhanghaoyu01 <zhanghaoyu01@zhuanzhuan.com>
2025-05-08 10:07:49 +0800

    code

--- src/main/java/org/example/springmcpserver/config/McpConfig.java
+++ src/main/java/org/example/springmcpserver/config/McpConfig.java
@@ -12,29 +12,14 @@ import org.springframework.context.annotation.Configuration;
 public class McpConfig {
 
 
-//    @Bean
-//    public List<ToolCallback> getToolCallbacks(WeatherService service) {
-//        return List.of(ToolCallbacks.from(service));
-//    }
-
+    /** 构建工具回调
+     * @param weatherService
+     * @return
+     */
     @Bean
     public ToolCallbackProvider getToolCallbackProvider(WeatherService weatherService) {
-        return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(weatherService)
+                .build();
     }
-
-    @Bean
-    public ToolCallbackProvider weatherTools(WeatherService weatherService) {
-        return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
-    }
-
-//    public record TextInput(String input) {
-//    }
-//
-//    @Bean
-//    public ToolCallback toUpperCase() {
-//        return FunctionToolCallback.builder("toUpperCase", (TextInput input) -> input.input().toUpperCase())
-//                .inputType(TextInput.class)
-//                .description("Put the text to upper case")
-//                .build();
-//    }
 }
--- src/main/java/org/example/springmcpserver/service/WeatherService.java
+++ src/main/java/org/example/springmcpserver/service/WeatherService.java
@@ -5,10 +5,17 @@ import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 
+/**
+ * 正常的spring service
+ */
 @Service
 public class WeatherService {
 
 
+    /** 构建根据城市获取天气的tool
+     * @param city 城市名称
+     * @return 天气信息
+     */
     @Tool(name = "getWeather", description = "根据城市获取天气")
     public String getWeather(String city) {
         return new String((city).getBytes(), StandardCharsets.UTF_8) + " 天气为晴天 25℃";
